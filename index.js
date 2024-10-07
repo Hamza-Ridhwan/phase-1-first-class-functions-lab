aa9a07c6ec1ed0f24d470b3230c9343b20028513
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-10-07 13:14:17 +0300

    Completed first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (scuberDrivers) {
+    return scuberDrivers.slice (0, 2)    
+}
+returnFirstTwoDrivers (['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = function (scuberDrivers) {
+    return scuberDrivers.slice (2, 4)    
+}
+returnLastTwoDrivers (['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+console.log(selectingDrivers [0](scuberDrivers))
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+const fareDoubler = createFareMultiplier(2);  
+const fareTripler = createFareMultiplier(3);  
+
+function selectDifferentDrivers(scuberDrivers, selectingDrivers){
+    return selectingDrivers(scuberDrivers)
+}
